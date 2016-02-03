1bc9deb1ff4b3452fe680ab3cb9cab76ff0545f9
ozzieozumo <luke.d.everett@gmail.com>
2016-02-03 16:09:04 -0500

    setup main storyboard

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Luke Everett on 2/2/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
