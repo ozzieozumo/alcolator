29d9dcfdbcb6b4e9465a3f3968af13ed87b2aee4
ozzieozumo <luke.d.everett@gmail.com>
2016-02-04 12:59:09 -0500

    cp21 add nav controller and segues

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Luke Everett on 2/4/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
